2c588c94dd170a93e418455728fbb6e1a3874b33
Brendon So <132183729+brendonilx@users.noreply.github.com>
2024-06-29 17:06:32 -0400

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,184 @@
+﻿/*
+    Brendon So
+    Copyright 2024
+    Umass Lowell
+    email: Brendon_So@student.uml.edu
+
+
+
+	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+	find() function**
+*/
+
+//----------------------------------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------------//
+
+const data = {
+	"pieces": [
+		{ "letter": "A", "value": 1, "amount": 9 },
+		{ "letter": "B", "value": 3, "amount": 2 },
+		{ "letter": "C", "value": 3, "amount": 2 },
+		{ "letter": "D", "value": 2, "amount": 4 },
+		{ "letter": "E", "value": 1, "amount": 12 },
+		{ "letter": "F", "value": 4, "amount": 2 },
+		{ "letter": "G", "value": 2, "amount": 3 },
+		{ "letter": "H", "value": 4, "amount": 2 },
+		{ "letter": "I", "value": 1, "amount": 9 },
+		{ "letter": "J", "value": 8, "amount": 1 },
+		{ "letter": "K", "value": 5, "amount": 1 },
+		{ "letter": "L", "value": 1, "amount": 4 },
+		{ "letter": "M", "value": 3, "amount": 2 },
+		{ "letter": "N", "value": 1, "amount": 5 },
+		{ "letter": "O", "value": 1, "amount": 8 },
+		{ "letter": "P", "value": 3, "amount": 2 },
+		{ "letter": "Q", "value": 10, "amount": 1 },
+		{ "letter": "R", "value": 1, "amount": 6 },
+		{ "letter": "S", "value": 1, "amount": 4 },
+		{ "letter": "T", "value": 1, "amount": 6 },
+		{ "letter": "U", "value": 1, "amount": 4 },
+		{ "letter": "V", "value": 4, "amount": 2 },
+		{ "letter": "W", "value": 4, "amount": 2 },
+		{ "letter": "X", "value": 8, "amount": 1 },
+		{ "letter": "Y", "value": 4, "amount": 2 },
+		{ "letter": "Z", "value": 10, "amount": 1 }
+	] //tried parse/fetch but it didn't work to extract the data from the json file 
+};
+
+//----------------------------------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------------//
+
+function Game(score) {
+	this.score = this.score;
+
+
+}
+
+function Tile(letter, value, amount) {
+	this.letter = letter;
+	this.value = value;
+	this.amount = amount;
+	this.image = `images/Scrabble_Tiles/Scrabble_Tile_${letter}.jpg`;
+}
+var total = 0;
+var gamesplayed = 1;
+var points = 0;
+var tpoints = 0;
+var val;
+
+function draw(letter) {
+
+	const obj = data.pieces[letter];
+
+	const tile = new Tile(obj.letter, obj.value, obj.amount);
+
+	const img = document.createElement('img');
+	img.src = tile.image;
+	img.classList.add('box');
+	total += tile.value;
+
+	
+	//https://stackoverflow.com/questions/1324044/how-do-i-disable-a-jquery-ui-draggable
+	//original method x/y axis breaks the generate
+	//https://jqueryui.com/draggable/#revert https://jqueryui.com/droppable/ 
+	//https://api.jquery.com/data/ ** review
+	val = tile.value;
+	$(img).draggable({ revert: true });
+	$(img).data('val', tile.value);
+	$(".board").data('val', tile.value);
+	$(".double").data('val', tile.value);
+
+	$("double").droppable({ //2x not working
+		drop: function (event, ui) {
+			val = ui.draggable.data('val');
+			tpoints += val * 2; //current
+			points += val * 2; //total
+
+			document.getElementById("cscore").innerHTML = tpoints;
+			tile.draggable('disable')
+		}
+	});
+
+	$(".board").droppable({
+		drop: function (event, ui) {
+			val = ui.draggable.data('val');
+			tpoints += val; //current
+			points += val; //total
+			
+			document.getElementById("cscore").innerHTML = tpoints;
+			tile.draggable('disable')
+		}
+	});
+
+
+
+	return img;
+}
+
+
+//----------------------------------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------------//
+//https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function generate() {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const randomIndex = Math.floor(Math.random()*26); //a-z w/ 27 max
+	return randomIndex;
+}
+
+var key = false;
+var extra = true;
+
+function print() { 
+	const tileRack = document.getElementById('rack');
+	tpoints = 0;
+
+	if (key == true) {
+		deletetiles(extra);
+		drawtiles();
+
+	}
+	else {
+		drawtiles();
+		key = true;
+	}
+	document.getElementById("score").innerHTML = total;
+	document.getElementById("tscore").innerHTML = points;
+	document.getElementById("cscore").innerHTML = tpoints;
+	document.getElementById("Games").innerHTML = gamesplayed;
+
+	total = 0;
+	
+	gamesplayed++;
+}
+
+function deletetiles() {
+	const tileRack = document.getElementById('rack');
+	for (var i = 0; i < 7; i++) {
+		tileRack.removeChild(tileRack.firstChild);
+	}
+	if (extra == true) {
+		tileRack.removeChild(tileRack.firstChild);
+		extra = false; 
+    }
+
+}
+
+function drawtiles() {
+	const tile = document.getElementById('rack');
+	const tileA = draw(generate());
+	tile.appendChild(tileA);
+	const tileB = draw(generate());
+	tile.appendChild(tileB);
+	const tileC = draw(generate());
+	tile.appendChild(tileC);
+	const tileD = draw(generate());
+	tile.appendChild(tileD);
+	const tileE = draw(generate());
+	tile.appendChild(tileE);
+	const tileF = draw(generate());
+	tile.appendChild(tileF);
+	const tileG = draw(generate());
+	tile.appendChild(tileG);
+}
---
+++ ReadMe.txt
@@ -0,0 +1,20 @@
+My scrabble website starts with the user pressing the button of which generates random letters 
+data which then displays the letter but also all of the letters values which represent the total 'max' score.
+The images are then added to the 'rack'. The images are draggable and can be placed on the board, 
+the images will revert back to the original position if not placed on the boards specific area,
+if the image is placed on the board, the image would stay and become un-draggable,
+The score board is then updated each time when the letter is placed,
+the score is accurate and based on each individual letter's data value.
+
+When the button is pressed again the data saved are the players score, which accumlates every game, 
+the board then removes and generates a new set of random letters and the game can be played again.
+
+Things that do not work, most importantly I was unable to implement the actual word checker, I could not comphrehend how to check if words
+are real words or not, I guess I could have created a small list of words then generate letters from the list but its too late now. My double implementation
+also does not function (the multiplyers) but the scores still add up correctly without multiplyers based on the data. Another is that the board where the 
+letters are placed is a single div, so the board itself has no logic implemented when it comes to letters and words.
+so in short the game generates random letters with values, its then placed on the board where the values add to the score, certain data from the 
+game is saved like total score and games played. I just could not implement a method to check words this late it took many days to get this far.
+
+Github account: https://github.com/brendonilx
+Website link: 
